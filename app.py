2a998136f5d22f35620f58eda6bf7a9401d453af
shreayan98c <shreayan98c@gmail.com>
2023-05-06 03:24:07 -0400

    Send form data to dashboard

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
@@ -6,3 +6,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template('home.html')
+
+
+@app.route("/dashboard", methods=['GET', 'POST'])
+def dashboard():
+    if request.method == 'POST':
+        input_url = request.form.get('input_url')
+        return render_template('dashboard.html', input_url=input_url)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
